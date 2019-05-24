7e6dd6df83bdf9c029cc94e0ac17a68d971b8423
Carlos Villanueva <ceva_19@hotmail.com>
2019-05-24 15:39:11 -0300

    Java Apache Maven Course

--- hello/src/main/java/helloworld.java
+++ hello/src/main/java/helloworld.java
@@ -1,7 +1,9 @@
+import org.apache.commons.lang3.StringUtils;
 public class helloworld
 {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello world");
+		System.out.println(StringUtils.capitalize("hello world"));
 	}
 } 
---
+++ mvn-ch04-gettingstarter-01/src/main/java/com/ceva/mvn/App.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ceva.mvn;
+import org.apache.commons.lang3.StringUtils;
+/**
+ *
+ * @author PC
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println(StringUtils.capitalize("hello world"));
+    }
+}
